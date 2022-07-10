582ebd78de2b366d8df9f9535ff95dc7defdd8e7
vaibhav <prince123Tiwari@outlook.com>
2022-07-09 21:27:23 -0700

    second.php

--- webFiles/login.php
+++ webFiles/login.php
@@ -66,7 +66,7 @@ while($row=mysqli_fetch_assoc($res)){
 else{
     echo"<script>alert('Oops no such accoun exist')</script>";
     header("location:./signup.php");
-}}
+}}  
 ?>
 
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
